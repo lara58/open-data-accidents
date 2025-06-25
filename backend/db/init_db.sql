8d9cc91ffb595deba11b14abef80a0acb3ed887d
Sidiki Kaba <sidiki.kaba@disinstruments.fr>
2025-06-25 18:45:03 +0200

    changement de type

--- backend/db/init_db.sql
+++ backend/db/init_db.sql
@@ -15,11 +15,11 @@ CREATE TABLE IF NOT EXISTS accidents (
     date_accident DATE NOT NULL,
     heure_accident TEXT,
     lieu_code_insee TEXT,
-    lieu_departement TEXT, --dep obligatoire pour la prediction (non numerique) 
+    lieu_departement TEXT, --dep obligatoire pour la prediction (numerique)
     lieu_commune TEXT,
     lieu_latitude DOUBLE,
     lieu_longitude DOUBLE,
-    agglomeration TEXT, --agg obligatoire pour la prediction (non numerique)
+    agglomeration TEXT, --agg obligatoire pour la prediction (numerique)
     type_route TEXT, --circ obligatoire pour la prediction (numerique)
     condition_meteo TEXT, --atm obligatoire pour la prediction (numerique)
     luminosite TEXT, --lum obligatoire pour la prediction (numerique)
@@ -28,12 +28,12 @@ CREATE TABLE IF NOT EXISTS accidents (
     nb_vehicules INTEGER,
     categorie_vehicule TEXT, --catv obligatoire pour la prediction (numerique)
     nb_usagers INTEGER,
-    categorie_usager TEXT,--catu obligatoire pour la prediction (non numerique)
+    categorie_usager TEXT,--catu oobligatoire pour la prediction (numerique)
     age INTEGER, --age obligatoire pour la prediction (numerique)
-    motif_deplacement TEXT, -- trajet obligatoire pour la prediction (non numerique)
-    equipement_securite TEXT, --secu1 obligatoire pour la prediction (non numerique)
+    motif_deplacement TEXT, -- trajet obligatoire pour la prediction (numerique)
+    equipement_securite TEXT, --secu1 obligatoire pour la prediction (numerique)
     place_usager TEXT, --place obligatoire pour la prediction (numerique)
-    sexe_usager TEXT, --sexe obligatoire pour la prediction (non numerique)
+    sexe_usager TEXT, --sexe obligatoire pour la prediction (numerique)
     manoeuvre_principal_accident TEXT, -- manv obligatoire pour la prediction (numerique)
     type_moteur TEXT, --motor obligatoire pour la prediction (numerique)
     vitesse_max INTEGER, --vma obligatoire pour la prediction (numerique)
